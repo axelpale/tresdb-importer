a7978c006d920a439e099c6700dcd91bc4f37095
Akseli Palen <akseli.palen@gmail.com>
2016-09-27 22:02:47 +0300

    Sample local.js

---
+++ config/local-sample.js
@@ -0,0 +1,20 @@
+module.exports = {
+
+  // Xitrux Locator MySQL settings
+  locator_db: {
+    host: 'localhost',
+    port: 1337,
+    user: 'foo',
+    password: '123456789',
+    database: 'locator'
+  },
+
+  // TresDB MongoDB settings
+  tresdb_db: {
+    host: 'localhost',
+    port: 27017,
+    user: 'bar',
+    password: '987654321',
+    database: 'tresdb'
+  }
+};
